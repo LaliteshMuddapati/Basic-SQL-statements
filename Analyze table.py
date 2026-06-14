f3a9e47b33a14681b7b1abd11f2f7aca24e787c1
LaliteshMuddapati <lallivedu@gmail.com>
2026-06-14 16:20:58 +0530

    Add files via upload

---
+++ Analyze table.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+database='database.sqlite'
+
+conn=sqlite3.connect(database)
+print('Opened data successfully')
+
+import pandas as pd
+tables=pd.read_sql("""SELECT FROM sqlite_master WHERE type'table';""", conn)
+
+print(tables)
+
+matches=pd.read_sql("""SELECT FROM Match;""", conn)
+
+print(matches.info())
